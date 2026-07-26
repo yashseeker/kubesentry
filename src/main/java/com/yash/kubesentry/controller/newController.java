d2e36eb1f3ac3b82f4858a413ff1bc4e5cfc201c
Yash Pandey <y97pandey@gmail.com>
2026-07-27 03:58:46 +0530

    Add root endpoint-2

--- src/main/java/com/yash/kubesentry/controller/newController.java
+++ src/main/java/com/yash/kubesentry/controller/newController.java
@@ -1,6 +1,7 @@
 package com.yash.kubesentry.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class newController {
 
     @GetMapping("/")
